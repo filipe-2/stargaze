fab0a099f2f01157b01e03247cdedd7441640d42
Carlos Filipe <carlosfilipesoaresramos2001@gmail.com>
2024-04-26 21:04:33 -0300

    feat(header): Add <hide header on scroll> functionality

---
+++ src/js/App.js
@@ -0,0 +1,8 @@
+import {
+    // Variáveis
+
+    // Funções
+    toggleHeaderVisibilityOnScroll,
+} from './modules/utils.js';
+
+window.addEventListener('scroll', toggleHeaderVisibilityOnScroll);
--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -1,1 +1,20 @@
+const header = document.querySelector('#js-header');
+let lastScrollTop = 0;
 
+// Função para esconder a barra de navegação ao rolar a tela
+function toggleHeaderVisibilityOnScroll() {
+    const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop && currentScrollTop > 0) {
+        header.classList.add('expanded');
+    } else header.classList.remove('expanded');
+
+    lastScrollTop = currentScrollTop;
+}
+
+export {
+    // Variáveis
+
+    // Funções
+    toggleHeaderVisibilityOnScroll,
+};
