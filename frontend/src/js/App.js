600ecfbc971fcef1ba01bcc1d498b68be1cafbfd
Carlos Filipe <carlosfilipesoaresramos2001@gmail.com>
2024-04-30 21:50:25 -0300

    refactor: Alterar padrão de diretórios

--- frontend/src/js/App.js
+++
@@ -1,9 +0,0 @@
-import {
-    // Variáveis
-
-    // Funções
-    toggleHeaderVisibilityOnScroll,
-} from './modules/utils.js';
-
-window.addEventListener('scroll', toggleHeaderVisibilityOnScroll);
-window.addEventListener('DOMContentLoaded', )
