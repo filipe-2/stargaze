787d8c7b5f6096421ba631d8abc21255d4a94ad7
Carlos Filipe <carlosfilipesoaresramos2001@gmail.com>
2024-05-01 18:22:02 -0300

    feat: Adicionar borda no título do hero e botão de fechar modal de missões

--- static/js/App.js
+++ static/js/App.js
@@ -6,3 +6,10 @@ import {
 } from './modules/utils.js';
 
 window.addEventListener('scroll', toggleHeaderVisibilityOnScroll);
+
+const closeModalBtn = document.querySelector('.missions__modal-close');
+const missionsModal = document.querySelector('.missions__modal-wrapper');
+
+closeModalBtn.addEventListener('click', () => missionsModal.classList.add('modal-closed'));
+
+console.log(closeModalBtn);
