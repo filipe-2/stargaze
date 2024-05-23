217cb6a8653da52d605c993cdbdd07e5d947f397
Will <williamcavalcante912@gmail.com>
2024-05-23 11:10:31 -0300

    Instalação do CORS
    o CORS foi instalado no arquivo __init__.py
    
    Co-Authored-By: Arthur Yvens  <117685054+GOW-GuanYu@users.noreply.github.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, template_folder='views', static_folder='public')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 db = SQLAlchemy(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 from app.models.missions import Missions
 with app.app_context():
-    db.create_all()
-
+    db.create_all()    
 @app.route('/') # Migrar para a pasta 'routes'
+@cross_origin()
 def index():
     return render_template('index.html')
