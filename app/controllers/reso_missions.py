18ad04cdf02548b896b88079a5f4bcc2bbc8775d
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-05-25 20:40:09 -0300

    feat: add GetMission endpoint to API

--- app/controllers/reso_missions.py
+++ app/controllers/reso_missions.py
@@ -5,3 +5,11 @@ from app.models.missions import Missions
 class Index(Resource):
     def get(self):
         return jsonify('Bem-vindo!')
+
+# Retorna erro para o cliente caso a missão não seja encontrada
+class GetMission(Resource):
+    def get(self, id):
+        mission = Missions.get_mission(self, id)
+        if(mission == None):
+            return {'message': 'Missão não cadastrada!'}, 404
+        return jsonify(mission)
