21db58439d5ffb311253c42a0cf6a3a120d565f5
Will <williamcavalcante912@gmail.com>
2024-05-27 14:14:18 -0300

    Implementação de métodos
    Métodos POST, PUT, DELETE e UPDATE adicionados

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
+from flask_restful import Api
 
 app = Flask(__name__, template_folder='views', static_folder='public')
+api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 db = SQLAlchemy(app)
 cors = CORS(app)
@@ -11,8 +13,15 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 from app.models.missions import Missions
 with app.app_context():
     db.create_all()   
-     
-@app.route('/') # Migrar para a pasta 'routes'
+
+from app.controllers.reso_missions import Index, MissionsCreate,MissionsUpdate, MissionsDelete
+api.add_resource(Index, '/get') 
+api.add_resource(MissionsCreate, '/create')
+api.add_resource(MissionsUpdate, '/update')
+api.add_resource(MissionsDelete, '/delete')
+
+@app.route('/',  methods=['GET', 'POST','DELETE','PUT']) # Mover para a pasta 'routes' e declarando métodos
 @cross_origin()
-def index():
+
+def Index():
     return render_template('index.html')
--- app/controllers/reso_missions.py
+++ app/controllers/reso_missions.py
@@ -1,7 +1,48 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from app.models.missions import Missions
+# Adicionando missões
+parser = reqparse.RequestParser()# Definição de argumentos
+parser.add_argument('name', required=True, help="Digite o nome")
+parser.add_argument('date_launch', required=True, help="Digite a data", type=int)
+
+# Atualizar missões
+parser_update = reqparse.RequestParser() 
+parser_update.add_argument('id', required=True, help="Digite o ID", type=int)
+parser_update.add_argument('name', required=True, help="Digite o nome")
+parser_update.add_argument('date_launch', required=True, help="Digite a data", type=int)
+
+#Deletar missões
+parser_delete = reqparse.RequestParser()
+parser_delete.add_argument('id', required= True, help="Digite o ID", type=int)
 
 class Index(Resource):
     def get(self):
-        return jsonify('Bem-vindo!')
+        return jsonify('Bem-vindo a stargaze!')
+    
+class MissionsCreate(Resource):
+    def post(self):
+        try:
+            datab = parser.parse_args()
+            Missions.save_missions(self, datab['name'], datab['date_launch'])
+            return {"message": 'Mission added!'}, 200
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
+
+class MissionsUpdate(Resource):
+    def put(self):
+        try:
+            datab = parser_update.parse_args()
+            Missions.update_missions(self, datab['id'], datab['name'], datab['date_launch'])
+            return {"message": 'Mission Updated!'}, 200    
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
+
+class MissionsDelete(Resource):
+    def delete(self):
+        try:
+            datab = parser_delete.parse_args()
+            Missions.delete_missions(self,datab['id'])
+            return {"message": 'Mission Deleted!'}, 200
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
