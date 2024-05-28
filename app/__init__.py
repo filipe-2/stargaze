668d3068244681b70702b47c40950814bda4b9b8
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-05-28 19:52:03 -0300

    feat: add "get" and "get all" endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,11 @@ from app.models.missions import Missions
 with app.app_context():
     db.create_all()   
 
-from app.controllers.reso_missions import Index, MissionsCreate,MissionsUpdate, MissionsDelete
-api.add_resource(Index, '/get') 
+from app.controllers.reso_missions import MissionsGetAll, MissionsCreate,MissionsUpdate, MissionsDelete, MissionsGet
+api.add_resource(MissionsGetAll, '/get')
+api.add_resource(MissionsGet, '/get/<int:id>')
 api.add_resource(MissionsCreate, '/create')
-api.add_resource(MissionsUpdate, '/update/<int:id>')#digitar Id da missão após /
+api.add_resource(MissionsUpdate, '/update/<int:id>') # o ID é passado na URL
 api.add_resource(MissionsDelete, '/delete/<int:id>')
 
 @app.route('/',  methods=['GET', 'POST','DELETE','PUT']) # Mover para a pasta 'routes' e declarando métodos
--- app/controllers/reso_missions.py
+++ app/controllers/reso_missions.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask_restful import Resource, reqparse
 from app.models.missions import Missions
 from datetime import datetime
@@ -7,10 +7,28 @@ from datetime import datetime
 def parse_date(x):
     return datetime.strptime(x, '%Y-%m-%d').date()
 
+# formata Missoes para JSON
+def parse_mission(mission):
+    mission = mission.__dict__
+    del mission['_sa_instance_state'] # remove instância inconvertível do banco de dados
+    mission['cost'] = float(mission['cost'])
+    mission['duration'] = f'{mission["duration"].days} days'
+    mission['cost'] = float(mission['cost'])
+    mission['date_launch'] = mission['date_launch'].strftime('%Y-%m-%d')
+    return mission
+
+def parse_mission_resume(mission):
+    mission = parse_mission(mission)
+    return {
+        'id': mission['id'], 'name': mission['name'], 'date_launch': mission['date_launch'],
+        'destiny': mission['destiny'], 'status': mission['status']
+    }
+
 # Adicionando missões
 parser = reqparse.RequestParser()# Definição de argumentos
 parser.add_argument('name', required=True, help="Type mission name")
 parser.add_argument('date_launch', required=True, help="Type mission date launch", type=parse_date)
+parser.add_argument('date_return', required=True, help="Type mission date return", type=parse_date) # a data de retorno é requerida para calcular a duração da missão
 parser.add_argument('destiny', required=True, help="Type a destiny", type=str)
 parser.add_argument('status', required=True, help="Type mission state", type=str)
 parser.add_argument('crew', required=True, help="Type mission crew", type=str)
@@ -18,26 +36,27 @@ parser.add_argument('load', required=True, help="Type mission load", type=str)
 parser.add_argument('cost', required=True, help="Type mission cost", type=float)
 parser.add_argument('status_details', required=True, help="Type mission status", type=str)
 
-class Index(Resource):
-    def get(self):
-        return jsonify('Bem-vindo a stargaze!')
-    
 class MissionsCreate(Resource):
     def post(self): 
-            datab = parser.parse_args()
+        try:
+            datab = parser.parse_args() 
+            datab['duration'] = datab['date_return'] - datab['date_launch'] # cálculo da duração da missão
             Missions.save_mission(
-                self, datab['name'], datab['date_launch'], datab['destiny'], 
-                datab['status'], datab['crew'], datab['load'], datab['cost'], datab['status_details']
+                self, datab['name'], datab['date_launch'], datab['destiny'], datab['status'],
+                datab['crew'], datab['load'], datab['duration'], datab['cost'], datab['status_details']
             )
-            return jsonify({"message": 'Mission added!'}), 200
+            return {"message": 'Mission added!'}, 200    
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
 
 class MissionsUpdate(Resource):
     def put(self, id):
         try:
             datab = parser.parse_args() 
+            datab['duration'] = datab['date_return'] - datab['date_launch'] # cálculo da duração da missão
             Missions.update_mission(
-                self, id, datab['name'], datab['date_launch'], datab['destiny'], 
-                datab['status'], datab['crew'], datab['load'], datab['cost'], datab['status_details']
+                self, id, datab['name'], datab['date_launch'], datab['destiny'], datab['status'],
+                datab['crew'], datab['load'], datab['duration'], datab['cost'], datab['status_details']
             )
             return {"message": 'Mission Updated!'}, 200    
         except Exception as e:
@@ -46,7 +65,25 @@ class MissionsUpdate(Resource):
 class MissionsDelete(Resource):
     def delete(self, id):
         try:
-            Missions.delete_mission(self,id)
+            Missions.delete_mission(self, id)
             return {"message": 'Mission Deleted!'}, 200
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
+
+class MissionsGet(Resource):
+    def get(self, id):
+        try:
+            mission = Missions.get_mission(self, id)
+            return parse_mission(mission), 200
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
+
+class MissionsGetAll(Resource):
+    def get(self):
+        try:
+            start_date = request.args.get('start_date')
+            end_date = request.args.get('end_date')
+            missions = Missions.get_all_missions(self, start_date, end_date)
+            return [parse_mission_resume(mission) for mission in missions], 200
         except Exception as e:
             return jsonify({'status': 500, 'msg': f'{e}'}), 500
