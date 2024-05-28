5534fbd19749e260dd45c2db69ace11f40a28d17
Will <williamcavalcante912@gmail.com>
2024-05-28 13:27:52 -0300

    adição get mission e correção de bug
    Def get_mission adicionado, correção na aba das missões e formato date corrigido.
    
    Co-Authored-By: Filipe Ramos <146658398+filipe-2@users.noreply.github.com>
    Co-Authored-By: Arthur Yvens  <117685054+GOW-GuanYu@users.noreply.github.com>

--- app/controllers/reso_missions.py
+++ app/controllers/reso_missions.py
@@ -1,17 +1,23 @@
 from flask import jsonify
 from flask_restful import Resource, reqparse
 from app.models.missions import Missions
+from datetime import datetime
+
+# formatador personalizado para datas
+def parse_date(x):
+    return datetime.strptime(x, '%Y-%m-%d').date()
+
 # Adicionando missões
 parser = reqparse.RequestParser()# Definição de argumentos
+parser.add_argument('id', required=True, help="Digite o ID", type=int)
 parser.add_argument('name', required=True, help="Digite o nome")
-parser.add_argument('date_launch', required=True, help="Digite a data", type=int)
+parser.add_argument('date_launch', required=True, help="Digite a data", type=parse_date)
 
-# Atualizar missões
+# Atualizar missões date_launch = datetime.strptime(args['date_launch'], '%Y-%m-%d') isso aqui, a gente tem q implementar isso
 parser_update = reqparse.RequestParser() 
 parser_update.add_argument('id', required=True, help="Digite o ID", type=int)
 parser_update.add_argument('name', required=True, help="Digite o nome")
-parser_update.add_argument('date_launch', required=True, help="Digite a data", type=int)
-
+parser_update.add_argument('date_launch', required=True, help="Digite a data", type=parse_date)
 #Deletar missões
 parser_delete = reqparse.RequestParser()
 parser_delete.add_argument('id', required= True, help="Digite o ID", type=int)
@@ -21,10 +27,10 @@ class Index(Resource):
         return jsonify('Bem-vindo a stargaze!')
     
 class MissionsCreate(Resource):
-    def post(self):
+    def post(self): 
         try:
             datab = parser.parse_args()
-            Missions.save_missions(self, datab['name'], datab['date_launch'])
+            Missions.save_missions(self, datab['id'], datab['name'], datab['date_launch'])
             return {"message": 'Mission added!'}, 200
         except Exception as e:
             return jsonify({'status': 500, 'msg': f'{e}'}), 500
@@ -32,7 +38,7 @@ class MissionsCreate(Resource):
 class MissionsUpdate(Resource):
     def put(self):
         try:
-            datab = parser_update.parse_args()
+            datab = parser_update.parse_args() 
             Missions.update_missions(self, datab['id'], datab['name'], datab['date_launch'])
             return {"message": 'Mission Updated!'}, 200    
         except Exception as e:
--- app/models/missions.py
+++ app/models/missions.py
@@ -1,20 +1,22 @@
 from app import db
 
 class Missions(db.Model):
+    
     __tablename__ = 'missions'
     __table_args__ = {'sqlite_autoincrement': True}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     date_launch = db.Column(db.Date)
 
-    # Inicializador
-    def __init__(self, name, date_launch):
+    # Inicializador 
+    def __init__(self, id, name, date_launch):
         self.name = name
-        self.data_lancamento = date_launch
+        self.date_launch = date_launch
+        self.id = id
     # Salvar missões
-    def save_missions(self, name, date_launch):
+    def save_missions(self, id, name, date_launch):
         try:
-            add_to_db = Missions(name, date_launch)
+            add_to_db = Missions(id, name, date_launch)
             print(add_to_db)
             db.session.add(add_to_db)
             db.session.commit() # Confirmar e salvar as alterações no banco de dados
@@ -39,3 +41,11 @@ class Missions(db.Model):
             print('Missão removida com sucesso.')
         except Exception as e:
             print('Ocorreu um erro ao remover a missão: ', e)
+    
+    def get_mission(self, id):
+        try:
+            mission = db.session.query(Missions).get(id)
+            print('Missão obtida com sucesso.')
+            return mission
+        except Exception as e:
+            print('Ocorreu um erro ao obter a missão: ', e)
